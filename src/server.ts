6c23917fe00421fafcb1c6e3ae5db67d6ab2d222
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-06-22 18:56:24 +0900

    feat:task-g

---
+++ src/server.ts
@@ -0,0 +1,3 @@
+//Architectural pattern: MVC, DI, (frontend)MVP  backentd suyagi butun boshli sklet toplami
+
+// Design pattern:Middleware, Decotar.  malum bir bolaklarni qol soyaki kabi
---
+++ train.js
@@ -0,0 +1,140 @@
+// TASK G:
+
+function getHighestIndex(son) {
+  let raqam = son[0];
+  let index = 0;
+
+  for (let i = 1; i < son.length; i++) {
+    if (son[i] > raqam) {
+      raqam == son[i];
+      index = i;
+    }
+  }
+
+  return console.log(index);
+}
+
+getHighestIndex([8, 23, 12, 21, 213, 3]);
+
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+/*       TASK-F
+function findDoublers(text) {
+  const result = text.split("");
+  for (let i = 0; i < result.length; i++) {
+    if (result.indexOf(result[i]) !== i) {
+      console.log(true);
+      return true;
+    }
+  }
+  console.log(false);
+  return false;
+}
+
+findDoublers("hello");
+*/
+
+//       TASK-E
+
+// function getReverse(text) {
+//   let result = text.split("").reverse().join("");
+//   console.log(result);
+// }
+
+// getReverse("hello");
+
+//       TASK-D
+// function checkContent(text1, text2) {
+//   let result1 = text1.split("");
+//   let result2 = text2.split("");
+
+//   result1.sort();
+//   result2.sort();
+
+//   return result1.join() == result2.join();
+// }
+
+// const result = checkContent("mitgroup", "gmtiprou");
+
+// console.log(result);
+
+// console.log(checkContent("hello", "world"));
+// console.log("====");
+
+//       TASK-C
+// const moment = require("moment");
+// class Shop {
+//   non;
+//   lagmon;
+//   cola;
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
+//   }
+
+//   qoldiq() {
+//     console.log(
+//       `hozir ${moment().format("HH:mm")} da ${this.non}ta non, ${
+//         this.lagmon
+//       }ta lagmon, ${this.cola}ta cola mavjud.`
+//     );
+//   }
+
+//   sotish(name, amount) {
+//     if (name === "non") {
+//       this.non -= amount;
+//     } else if (name === "lagmon") {
+//       this.lagmon -= amount;
+//     } else if (name === "cola") {
+//       this.cola -= amount;
+//     } else {
+//       console.log("bunday mahsulot mavjud emas");
+//     }
+//   }
+
+//   qabul(name, amount) {
+//     if (name === "non") {
+//       this.non += amount;
+//     } else if (name === "lagmon") {
+//       this.lagmon += amount;
+//     } else if (name === "cola") {
+//       this.cola += amount;
+//     } else {
+//       console.log("bunday mahsulot mavjud emas");
+//     }
+//   }
+// }
+// const shop = new Shop(6, 8, 4);
+// shop.qoldiq();
+// console.log("mahsulotlarimizni  yangilan royhati");
+// shop.sotish("non", 4);
+// shop.sotish("lagmon", 4);
+
+//shop.qoldiq();
+
+//shop.qabul("lagmon", 5);
+
+/*       TASK B      */
+// function countDigits(text) {
+//   const result = text.split("");
+//   const lastResult = result.filter((ele) => {
+//     return ele >= 0 && ele !== " ";
+//   });
+//   console.log(`${text} ning ichida raqamlar ${lastResult.length}ta bor`);
+// }
+// countDigits("ad2 a54f05y79w");
+
+/*       TASK A      */
+// function countDigits("ad2a54y79wet0sfgb9") {
+//   const result = misra.split();
+//   const lastResult = result.filter((e) => e === alfabit);
+//   console.log(`${misra} ning ichida ${alfabit} dan ${lastResult.length}ta bor`);
+// }
+// countLetter("e", "enginee");
