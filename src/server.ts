47b30974766ff05e877edd0e4430b258c228866f
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-06-23 15:32:16 +0900

    feat:integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,2 @@
-//Architectural pattern: MVC, DI, (frontend)MVP  backentd suyagi butun boshli sklet toplami
-
-// Design pattern:Middleware, Decotar.  malum bir bolaklarni qol soyaki kabi
+import dotenv from "dotenv";
+dotenv.config();
