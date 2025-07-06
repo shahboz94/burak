62dfdd13171e10a69490bf77f9b4c63b3131b9c3
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-07-06 18:59:49 +0900

    feat: create checkAuthSession api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,6 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
+import { Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 
@@ -74,6 +75,21 @@ restaurantController.processLogin = async function (
     });
   } catch (err) {
     console.error("Error, processLogin:", err);
+    res.send(err);
+  }
+};
+
+restaurantController.checkAuthSession = async (
+  req: AdminRequest,
+  res: Response
+) => {
+  try {
+    console.log("checkAuthSession ");
+    if (req.session?.member)
+      res.send(`<script> alert("${req.session.member.memberNick}")</script>`);
+    else res.send(`<script> alert("${Message.NOT_AUTHENTICATED}")</script>`);
+  } catch (err) {
+    console.error("Error, checkAuthSession:", err);
     res.send(err);
   }
 };
