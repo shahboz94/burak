70473bc597d2b1252faaa1bf82717e1fde7f4c5c
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-07-11 15:34:48 +0900

    fix: modify updateChosenProduct business logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -41,14 +41,14 @@ productController.createNewProduct = async (
     await productService.createNewProduct(data);
 
     res.send(
-      `<script> alert("Sucessful creation"); window. location. replace('admin/product/all')</script>`
+      `<script> alert("Sucessful creation"); window.location.replace('admin/product/all')</script>`
     );
   } catch (err) {
     console.error("Error, createNewProduct:", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window. location. replace('admin/product/all')</script>`
+      `<script> alert("${message}"); window.location.replace('admin/product/all')</script>`
     );
   }
 };
@@ -56,6 +56,10 @@ productController.createNewProduct = async (
 productController.updateChosenProduct = async (req: Request, res: Response) => {
   try {
     console.log("updateChosenProduct ");
+    const id = req.params.id;
+    const result = await productService.updateChosenProduct(id, req.body);
+
+    res.status(HttpCode.OK).json({ data: result });
   } catch (err) {
     console.error("Error, updateChosenProduct:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -65,7 +65,7 @@ restaurantController.processSignup = async (
     const message =
       err instanceof Error ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window. location. replace('admin/signup')</script>`
+      `<script> alert("${message}"); window.location.replace('admin/signup')</script>`
     );
   }
 };
@@ -90,7 +90,7 @@ restaurantController.processLogin = async function (
     const message =
       err instanceof Error ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window. location. replace('admin/login')</script>`
+      `<script> alert("${message}"); window.location.replace('admin/login')</script>`
     );
   }
 };
