ced3029bbe824764ff5e5f07fe06eefb09aaaeee
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-07-15 17:30:41 +0900

    feat:task

--- src/train.ts
+++ src/train.ts
@@ -1,22 +1,28 @@
 console.log("--- ---");
-//TASK M:
 
-function getSquareNumbers(arr: number[]) {
-  const result = arr.map((num) => ({
-    number: num,
-    square: num * num,
-  }));
-  console.log(result);
+//TASK N:
+
+function palindromCheck(text: string) {
+  let reversed_text = text.split("").reverse().join("");
+  let result = text === reversed_text;
+  console.log("Palindrom textmi?", result);
   return result;
 }
 
-getSquareNumbers([1, 2, 3]); //return [{ number: 1, square: 1 }, { number: 2, square: 4 }, { number: 3, square: 9 }];
+palindromCheck("dad");
+palindromCheck("anna");
+palindromCheck("function");
 
-// Shunday function tuzing, u raqamlardan tashkil topgan array qabul qilsin
-// va array ichidagi har bir raqam uchun raqamning o'zi va hamda o'sha raqamni kvadratidan
-// tashkil topgan object hosil qilib, hosil bo'lgan objectlarni array ichida qaytarsin
+// Parametr sifatida yagona string qabul qiladigan function tuzing.
+// Va bu function string'ni palindrom so'z yoki palindrom so'z emasligini aniqlab (boolean)
+// 'true' yokida 'false' qaytarsin.
 
-// MASALAN: MASALAN: getSquareNumbers([1, 2, 3]) return [{ number: 1, square: 1 }, { number: 2, square: 4 }, { number: 3, square: 9 }];
+// MASALAN: palindromCheck("dad") return true; palindromCheck("son") return false;
+// Birinchi misolda 'dad' so'zini ikkala tarafdan o'qilganda ham bir xil ma'noni beradi (true)
+// Ikkinchi misolda 'son' so'zini ikkala tarafdan o'qilganda bir xil ma'noni bermaydi (false)
+
+// *Palindrom so'z deb o'ngdan chapga ham ~ chapdan o'ngga ham o'qilganda
+// bir xil ma'noni beradigan so'zga aytiladi
 
 /*
 
@@ -85,6 +91,31 @@ reverseSentence("we like coding!");
 //Qaytayotgan natijaga e'tibor bersangiz, so'zlar joyi o'zgarmasdan turgan o'rnida teskarisiga o'girilmoqda
 
 */
+/*
+
+
+//TASK M:
+
+function getSquareNumbers(arr: number[]) {
+  const result = arr.map((num) => ({
+    number: num,
+    square: num * num,
+  }));
+  console.log(result);
+  return result;
+}
+
+getSquareNumbers([1, 2, 3]); //return [{ number: 1, square: 1 }, { number: 2, square: 4 }, { number: 3, square: 9 }];
+
+// Shunday function tuzing, u raqamlardan tashkil topgan array qabul qilsin
+// va array ichidagi har bir raqam uchun raqamning o'zi va hamda o'sha raqamni kvadratidan
+// tashkil topgan object hosil qilib, hosil bo'lgan objectlarni array ichida qaytarsin
+
+// MASALAN: MASALAN: getSquareNumbers([1, 2, 3]) return [{ number: 1, square: 1 }, { number: 2, square: 4 }, { number: 3, square: 9 }];
+
+/*
+
+
 
 /*
 
