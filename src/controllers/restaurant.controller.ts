afab2fdae64b4fe4e72c4afd52aa5f25952fa832
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-07-20 19:29:34 +0900

    fix: users page real data

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -115,7 +115,7 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
   try {
     console.log("getUsers ");
     const result = await memberService.getUsers();
-    console.log("result:", result);
+    //console.log("result:", result);
 
     res.render("users", { users: result });
   } catch (err) {
