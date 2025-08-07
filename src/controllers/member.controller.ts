d480858c59635a1a045207d2474a8b54ca8fe738
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-08-08 06:30:48 +0900

    feat: develop authentication via tokens with cookies

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 const memberService = new MemberService();
 const authService = new AuthService();
@@ -14,12 +15,15 @@ memberController.signup = async (req: Request, res: Response) => {
   try {
     console.log("signup ");
     const input: MemberInput = req.body,
-      result: Member = await memberService.signup(input),
-      token = await authService.createToken(result);
-    console.log("token =>", token);
+      result: Member = await memberService.signup(input);
+    const token = await authService.createToken(result);
+    res.cookie("accessToken", token, {
+      maxAge: AUTH_TIMER * 3600 * 1000,
+      httpOnly: false,
+    });
     //TODO:TOKENS AUTHENTICATION
 
-    res.json({ member: result });
+    res.status(HttpCode.OK).json({ member: result, accessToken: token });
   } catch (err) {
     console.error("Error, signup:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -33,11 +37,14 @@ memberController.login = async (req: Request, res: Response) => {
     const input: LoginInput = req.body,
       result = await memberService.login(input),
       token = await authService.createToken(result);
-    console.log("token =>", token);
 
+    res.cookie("accessToken", token, {
+      maxAge: AUTH_TIMER * 3600 * 1000,
+      httpOnly: false,
+    });
     //TODO:TOKENS AUTHENTICATION
 
-    res.json({ member: result });
+    res.status(HttpCode.OK).json({ member: result, accessToken: token });
   } catch (err) {
     console.error("Error, login:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
