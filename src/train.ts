81606020d4882d8660409ed15890948dd49da5f1
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2025-08-27 05:22:36 +0900

    feat: TASK ZD

--- src/train.ts
+++ src/train.ts
@@ -1,5 +1,31 @@
 console.log("--- ---");
+// TASK ZD
 
+function changeNumberInArray(
+  index: number,
+  arr: number[],
+  newValue: number
+): number[] {
+  const result = [...arr];
+
+  result[index] = newValue;
+  console.log(result);
+  return result;
+}
+
+changeNumberInArray(1, [1, 3, 7, 2], 2);
+
+// Shunday function yozing. Bu function o'ziga, parametr sifatida
+// birinchi oddiy number, keyin yagona array va uchinchi bo'lib oddiy number
+// qabul qilsin. Berilgan birinchi number parametr, arrayning tarkibida indeks bo'yicha hisoblanib,
+// shu aniqlangan indeksni uchinchi number parametr bilan alashtirib, natija sifatida
+// yangilangan arrayni qaytarsin.
+
+// MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2];
+
+// Yuqoridagi misolda, birinchi raqam bu '1' va arrayning '1'chi indeksi bu 3.
+// Bizning function uchinchi berilgan '2' raqamini shu '3' bilan almashtirib,
+// yangilangan arrayni qaytarmoqda.
 /*
 
 Project Standards:
